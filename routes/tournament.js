96ec1ca57f162562b60afc92cffdacf7e84506d4
applimetis <office@applimetis.id>
2022-10-16 23:25:20 +0700

    update

--- routes/tournament.js
+++ routes/tournament.js
@@ -2,6 +2,7 @@ const routes = require('express').Router()
 const TournamentController = require('../controllers/tournaments')
 
 routes.get('/', TournamentController.getAll)
+routes.get('/leaderboard', TournamentController.getLeaderBoard)
 routes.post('/:position', TournamentController.postResult)
 routes.put('/:position', TournamentController.updateResult)
 routes.delete('/:tournament_id/:position', TournamentController.deleteResult)
