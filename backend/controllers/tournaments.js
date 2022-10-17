287d8d54aa3b8a24882cc301e0d2295538889813
applimetis <office@applimetis.id>
2022-10-17 16:29:19 +0700

    heroku deployed

--- backend/controllers/tournaments.js
+++ backend/controllers/tournaments.js
@@ -222,7 +222,7 @@ class TournamentController {
           model: User
         }],
         order: [["total_point", "DESC"]],
-        group: [["Team.id", "User.id", "Tournament_results.id"]]
+        group: ["Tournament_results.id", "Team.id", "User.id" ]
       }
 
       const data = await Team.findAll(option)
