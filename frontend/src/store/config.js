86051aadf2e074875d87a9dc6645f9c49b594196
applimetis <office@applimetis.id>
2022-10-17 15:19:02 +0700

    set heroku frontend

--- frontend/src/store/config.js
+++ frontend/src/store/config.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 const port = 3000
+const url = "https://metacotest.herokuapp.com"
 
 const appApi = axios.create({
-    baseURL: `http://localhost:${port}`
+    baseURL: process.env.NODE_ENV ? url : `http://localhost:${port}`
 })
 
 export default appApi
